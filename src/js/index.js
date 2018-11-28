74eee1f00bd9f016cd49b7fbb1bf64be88e1ef73
Charles Young <young.charles90@gmail.com>
2018-11-28 15:11:35 -0600

    Add Snow and fix div overflow.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import ScrollTrigger from 'scrolltrigger-classes/ScrollTrigger.min';
 
 window.onload = () => {
-    console.log("OnLoad")
+    console.log("Loaded")
     new ScrollTrigger({
         toggle: {
             visible: 'reveal-text',
@@ -20,8 +20,10 @@ window.onload = () => {
         name[i].classList.add('reveal-text');
         name[i].classList.remove('invisible');
     }
-    let content = document.querySelector(".content");
-    content.classList.add('loaded');
+    let fade = document.querySelectorAll(".fade-in");
+    for (let i = 0; i < fade.length; i++) {
+        fade[i].classList.add('loaded');
+    }
 
     window.mobileAndTabletcheck = () => {
         let check = false;
@@ -67,5 +69,120 @@ window.onload = () => {
             initCursor = false;
         };
     }
+
+    //snow
+    (() => {
+        window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+
+    let flakes = [],
+        canvas = document.getElementById("canvas"),
+        ctx = canvas.getContext("2d"),
+        flakeCount = 400,
+        mX = -100,
+        mY = -100;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    function snow() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < flakeCount; i++) {
+            let flake = flakes[i],
+                x = mX,
+                y = mY,
+                minDist = 150,
+                x2 = flake.x,
+                y2 = flake.y;
+
+            let dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
+
+            if (dist < minDist) {
+                let force = minDist / (dist * dist),
+                    xcomp = (x - x2) / dist,
+                    ycomp = (y - y2) / dist,
+                    deltaV = force / 2;
+
+                flake.velX -= deltaV * xcomp;
+                flake.velY -= deltaV * ycomp;
+
+            } else {
+                flake.velX *= .98;
+                if (flake.velY <= flake.speed) {
+                    flake.velY = flake.speed
+                }
+                flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+            }
+
+            ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+            flake.y += flake.velY;
+            flake.x += flake.velX;
+
+            if (flake.y >= canvas.height || flake.y <= 0) {
+                reset(flake);
+            }
+
+
+            if (flake.x >= canvas.width || flake.x <= 0) {
+                reset(flake);
+            }
+
+            ctx.beginPath();
+            ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
+        requestAnimationFrame(snow);
+    }
+
+    function reset(flake) {
+        flake.x = Math.floor(Math.random() * canvas.width);
+        flake.y = 0;
+        flake.size = (Math.random() * 3) + 2;
+        flake.speed = (Math.random()) + 0.5;
+        flake.velY = flake.speed;
+        flake.velX = 0;
+        flake.opacity = (Math.random() * 0.5) + 0.3;
+    }
+
+    function init() {
+        for (let i = 0; i < flakeCount; i++) {
+            let x = Math.floor(Math.random() * canvas.width),
+                y = Math.floor(Math.random() * canvas.height),
+                size = (Math.random() * 3) + 2,
+                speed = (Math.random()) + 0.5,
+                opacity = (Math.random() * 0.5) + 0.3;
+
+            flakes.push({
+                speed: speed,
+                velY: speed,
+                velX: 0,
+                x: x,
+                y: y,
+                size: size,
+                stepSize: (Math.random()) / 30,
+                step: 0,
+                opacity: opacity
+            });
+        }
+
+        snow();
+    }
+
+    document.addEventListener("mousemove", (e) => {
+        mX = e.clientX;
+        mY = e.clientY;
+    });
+
+    window.addEventListener("resize", () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    });
+
+    init();
 };
 
